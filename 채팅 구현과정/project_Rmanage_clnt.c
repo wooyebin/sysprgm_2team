8b1f46669031c5bac7a376c28043d23872e1aadf
hdh4202 <43611344+DongHyeonHDH@users.noreply.github.com>
2021-12-23 23:49:37 +0900

    Update project_Rmanage_clnt.c
    잘 모르겠는 부분 있으면 알려주세요!!

--- 채팅 구현과정/project_Rmanage_clnt.c
+++ 채팅 구현과정/project_Rmanage_clnt.c
@@ -4,106 +4,76 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <semaphore.h>
 #include <pthread.h>
+#include <sys/wait.h>
+
 	
 #define BUF_SIZE 400
 #define NAME_SIZE 20
+#define PORTNUMBER "9000"
+#define CHATCLNTPORT "9005"
 
 typedef struct {
     char roomName[20]; //chatroom name
     int cnt; //people count
     int roomnum; //chatroom portnum
+    int option; //to check enter or make room
 }roominfo;
 
-void logInPage();
-void showmenu(char name[]);
+void logInPage(int sock, int chatclnt_sock);
+void showmenu(int sock, int chatclnt_sock);
 void help();
-void * send_msg(void * arg);
-void * recv_msg(void * arg);
 void error_handling(char * output);
-void createRoom(int sock, int chatclnt_sock,roominfo rinfo,char name[]);    
-void enterRoom(int sock, char name[]);   
+void createRoom(int sock, int chatclnt_sock, roominfo rinfo);    
+void enterRoom(int sock, int chatclnt_sock);   
+void chatting(int chatclnt_sock,roominfo rinfo);
 
-char name[NAME_SIZE]="[DEFAULT]";
 char output[BUF_SIZE];
+char msg[BUF_SIZE];
+char name[NAME_SIZE];
 roominfo info;
+static sem_t sem_one;
+static sem_t sem_two;
 	
 int main()
 {
 	int sock;
     int chatclnt_sock;
 
-	int clnt_adr_sz;
-	struct sockaddr_in serv_addr, chatclnt_addr;
-	
+	int chatclnt_adr_sz;
+	struct sockaddr_in serv_addr, chatclnt_adr;
+	// serv _addr -> to connect Rmanage_serv
+    // chatclnt_adr -> to connect chat_serv
 	
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	chatclnt_sock = socket(PF_INET, SOCK_STREAM, 0);
 
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));    
+	serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	serv_addr.sin_port=htons(atoi(PORTNUMBER));    
 	
 	
-	memset(&chatclnt_adr, 0, sizeof(chatclnt_adr));
+    memset(&chatclnt_adr, 0, sizeof(chatclnt_adr));
 	chatclnt_adr.sin_family = AF_INET; 
-	chatclnt_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	chatclnt_adr.sin_port = htons(atoi(argv[1]));
+	serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	chatclnt_adr.sin_port = htons(atoi(CHATCLNTPORT));
 	
-	if(bind(chatclnt_sock, (struct sockaddr*) &chatserv_adr, sizeof(serv_adr))==-1)
-		error_handling("bind() error");
+    printf("\nRmanage_client start\n");   
+    
 
-	if(listen(chatclnt_sock, 5)==-1)
-		error_handling("listen() error");	
-	  
 	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
 		error_handling("connect() error");
 	
-    logInPage();
+    logInPage(sock, chatclnt_sock);
     
 
-    close(chatclnt_sock);
+    //close(chatclnt_sock);
 	close(sock);  
 	return 0;
 }
-/*	
-void * send_msg(void * arg)   // send thread main
-{
-	int sock=*((int*)arg);
-	char name_output[NAME_SIZE+BUF_SIZE];
-	while(1) 
-	{
-        logInPage();
-           
-		fgets(output, BUF_SIZE, stdin);
-		if(!strcmp(output,"q\n")||!strcmp(output,"Q\n")) 
-		{
-			close(sock);
-			exit(0);
-		}
-		sprintf(name_output,"%s %s", name, output);
-		write(sock, name_output, strlen(name_output));
-	}
-	return NULL;
-}
-	
-void * recv_msg(void * arg)   // read thread main
-{
-	int sock=*((int*)arg);
-	char name_output[NAME_SIZE+BUF_SIZE];
-	int str_len;
-	while(1)
-	{
-		str_len=read(sock, name_output, NAME_SIZE+BUF_SIZE-1);
-		if(str_len==-1) 
-			return (void*)-1;
-		name_output[str_len]=0;
-		fputs(name_output, stdout);
-	}
-	return NULL;
-}
-*/	
+
 void error_handling(char *output)
 {
 	fputs(output, stderr);
@@ -111,16 +81,16 @@ void error_handling(char *output)
 	exit(1);
 }
 
-void logInPage(){
-    //enter NickName
-    char name[NAME_SIZE];
+void logInPage(int sock, int chatclnt_sock){
+
+    //enter NickName    
     printf("Show your NickName : ");
     fgets(name, BUF_SIZE, stdin);
 
-    showmenu(name);
+    showmenu(sock, chatclnt_sock);
 }
 
-void showmenu(char name[]){
+void showmenu(int sock, int chatclnt_sock){
     int menu = 0;
     printf("1. Create Room\n");
     printf("2. Enter Room\n");
@@ -136,17 +106,20 @@ void showmenu(char name[]){
     }
     
     else if(menu == 1){
-        createRoom(name);
+        createRoom(sock, chatclnt_sock, info);
     }
     else if(menu == 2){
-        enterRoom(name);
+        enterRoom(sock, chatclnt_sock);
     }
     else if(menu == 3){
         printf("terminate chatting room program\n");
         exit(1);
     }
-    else (menu == 4){
-        Help();
+    else if(menu == 4){
+        help();
+    }
+    else {
+        printf("error in menu \n");
     }
 
 }
@@ -158,66 +131,153 @@ void help(){
     
     while(fgets(buf, sizeof(buf), fp) != NULL){
         printf("%s", buf);
-        memset(buf,0,sizeof(buf));
+        memset(buf, 0, sizeof(buf));
     }
     fclose(fp);
 
 }
 
-void createRoom(int sock, int chatclnt_sock,roominfo rinfo,char name[]){    
-    int str_len;
+void createRoom(int sock, int chatclnt_sock,roominfo rinfo){    
+    int status;
+    int str_len = 0;
     int pid;   
     char roomname[NAME_SIZE];
-    pid = fork();
-
-    if((pid = fork())<0){
-        printf("error fork\n");
-        exit(0);
-    }
+    
     //enter roomname
     //child process, receive portnum from Rmanage_serv.c
-    else if(pid == 0){          
-        //input roomName
-        printf("input your roonName : ");
-        fgets(roomname, NAME_SIZE, stdin);        
-        strcpy(rinfo.roomName, roomname);
-        //send to rmanage server
-        write(sock, (void*) &rinfo, sizeof(info));
+    
+    //input roomName
+    printf("input your roomName to create: ");
+    scanf("%s", roomname);
+    //fgets(roomname, NAME_SIZE, stdin);        
+    
+    strcpy(rinfo.roomName, roomname);
 
-        //for receive rmanage server           
-        str_len = read(sock, (void*)&rinfo, sizeof(info));
-        if(str_len==-1){
-            error_handling("roominfo error");
-        }
-        // for testing
-        printf("recv roomnum : %d", rinfo.roomnum);        
+    //input option
+    rinfo.option = 1;
+
+    //send to rmanage server
+    write(sock, (void*)&rinfo, sizeof(info));
+
+    //for receive rmanage server           
+    str_len = read(sock, (void*)&rinfo, sizeof(info));
+    if(str_len == -1){
+        error_handling("roominfo error");
     }
-    //parent process, send roominfo to chatserv.c  
-    else{              
-        int status;
 
-        wait(&status);        
-        if(WIFEXITED(status)){                     
-            
-            write(chatclnt_sock, (void*) &rinfo, sizeof(info));             
-            // for testing
-            printf("write to chatclnt roomnum : %d ", rinfo.roomnum); 
+    // for testing
+    printf("recv roomnum : %d", rinfo.roomnum);        
+    // because it is tcp, we have two option 1,2
+    close(sock);    
+    
 
-            //enter to room which we create 
-
-        }
-    }
+    //start chat
+    //chatting(chatclnt_sock, rinfo);
 }
-void enterRoom(int sock, char name[]){    
+
+void enterRoom(int sock, int chatclnt_sock){    
     char enterRoomName[NAME_SIZE];   
+    int str_len;
+    roominfo sendinfo;
 
     //input roomName
-    printf("input your roonName : ");
-    fgets(enterRoomName, NAME_SIZE, stdin);        
+    printf("input your roomName to enter : ");
+
+    //fgets(enterRoomName, NAME_SIZE, stdin);          
+    scanf("%s", enterRoomName);
+    strcpy(sendinfo.roomName, enterRoomName);
     
-    strcpy(enterRoomName, name);
+    sendinfo.option = 2;    
 
-    //enter to room by using strcmp()
-    //so we modify Rmanage serv.c
+    write(sock, (void*) &sendinfo, sizeof(sendinfo));
 
+    printf("\n loading......\n");
+    sleep(1);
+
+    str_len = read(sock, (void*)&sendinfo, sizeof(sendinfo));
+    if(str_len==-1){
+        error_handling("roominfo error");
+    }
+    
+    if(sendinfo.roomnum == -1){
+        printf("this chatting room name is not in room information.\n"); 
+    }
+    else{
+        printf("write to chatclnt roomnum : %d ", sendinfo.roomnum);     
+    }
+    
+    close(sock);
+    //start chat
+
+    //chatting(chatclnt_sock, sendinfo);
 }
+
+/*
+void chatting(int chatclnt_sock,roominfo rinfo){
+
+    if(connect(chatclnt_sock, (struct sockaddr*)&chatclnt_adr, sizeof(chatclnt_adr))==-1)
+	    error_handling("connect() error");
+    
+    int str_len;
+    pthread_t snd_thread, rcv_thread;
+	void * thread_return;
+    sem_init(&sem_one, 0, 0);
+	sem_init(&sem_two, 0, 1);    
+
+    printf("\nstart chatting welcome %s\n\n", name);    
+    printf("say hello to new participator %s!!\n", name);        
+
+    pthread_create(&snd_thread, NULL, send_msg, (void*)&chatclnt_sock);
+    pthread_create(&rcv_thread, NULL, recv_msg, (void*)&chatclnt_sock);
+
+    pthread_join(snd_thread, &thread_return);
+    pthread_join(rcv_thread, &thread_return);
+
+    close(chatclnt_sock);  
+    sem_destroy(&sem_one);
+    sem_destroy(&sem_two);
+    
+
+    
+}
+
+void * send_msg(void * arg){
+	
+	int sock=*((int*)arg);
+	char name_msg[NAME_SIZE+BUF_SIZE];
+
+	while(1) 
+	{
+		sem_wait(&sem_two);
+		fgets(msg, BUF_SIZE, stdin);
+		if(!strcmp(msg,"q\n")||!strcmp(msg,"Q\n")) 
+		{
+			close(sock);
+			exit(0);
+		}
+		sprintf(name_msg,"%s %s", name, msg);
+		write(sock, name_msg, strlen(name_msg));
+		sem_post(&sem_one);
+	}
+	return NULL;
+}
+	
+void * recv_msg(void * arg){
+	int sock=*((int*)arg);
+	char name_msg[NAME_SIZE+BUF_SIZE];
+	int str_len;
+	while(1)
+	{
+		sem_wait(&sem_one);
+		str_len=read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
+		if(str_len==-1) 
+			return (void*)-1;
+		name_msg[str_len]=0;
+		fputs(name_msg, stdout);
+		sem_post(&sem_two);
+		
+	}
+	return NULL;
+}
+
+*/
