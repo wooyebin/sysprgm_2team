b39754c8aa1dc57a985f895b7077b1e4b3f39695
wooyebin <ye162200@gmail.com>
2021-12-23 04:33:38 +0000

    server function

--- server.c
+++ server.c
@@ -1,3 +1,9 @@
+/* System Programming Team 2
+ * OS : Ubuntu 18.04
+ * complier : gcc 7.5.0
+ * file : server.c
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,43 +16,62 @@
 #define BUF_SIZE 100
 #define MAX_CLNT 256
 
+void init(int, char**);
+void socket_init(int*);
+void chat_start(int*);
 void * handle_clnt(void * arg);
 void send_msg(char * msg, int len);
 void error_handling(char * msg);
-int command_detection(char*);
+//int command_detection(char*);
 
+int port;
 int clnt_cnt=0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mutx;
 
 int main(int argc, char *argv[])
 {
-	int serv_sock, clnt_sock;
-	struct sockaddr_in serv_adr, clnt_adr;
-	int clnt_adr_sz;
-	pthread_t t_id;
+	int serv_sock;
+	init(argc, argv);
+	socket_init(&serv_sock);	
+	chat_start(&serv_sock);	
+	close(serv_sock);
+	return 0;
+}
+
+
+
+///////////////////////////////////////////////////////////
+
+void init(int argc, char** argv){
 	if(argc!=2) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
-  
-	pthread_mutex_init(&mutx, NULL);
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
-
+	port = atoi(argv[1]);
+}
+void socket_init(int* serv_sock){
+	struct sockaddr_in serv_adr;
+	*serv_sock=socket(PF_INET, SOCK_STREAM, 0);
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family=AF_INET; 
 	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_adr.sin_port=htons(atoi(argv[1]));
-	
-	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
+	serv_adr.sin_port=htons(port);
+	if(bind(*serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
 		error_handling("bind() error");
-	if(listen(serv_sock, 5)==-1)
+	if(listen(*serv_sock, 5)==-1)
 		error_handling("listen() error");
-	
+}
+void chat_start(int* serv_sock){
+	int clnt_sock;
+	struct sockaddr_in clnt_adr;
+	int clnt_adr_sz;
+	pthread_t t_id;
+	pthread_mutex_init(&mutx, NULL);
 	while(1)
 	{
 		clnt_adr_sz=sizeof(clnt_adr);
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+		clnt_sock=accept(*serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
 		
 		pthread_mutex_lock(&mutx);
 		clnt_socks[clnt_cnt++]=clnt_sock;
@@ -56,10 +81,8 @@ int main(int argc, char *argv[])
 		pthread_detach(t_id);
 		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
 	}
-	close(serv_sock);
-	return 0;
 }
-	
+
 void * handle_clnt(void * arg)
 {
 	int clnt_sock=*((int*)arg);
@@ -87,15 +110,17 @@ void * handle_clnt(void * arg)
 void send_msg(char * msg, int len)   // send to all
 {
 	int i;
-
-	int commandNum = command_detection(msg);
-	if(commandNum == 1) printf("NOTICE\n");
+/*	int commandNum = command_detection(msg);
+	if(commandNum == 1){
+		strcpy(msg, "notice is uploaded");
+		len = strlen(msg);
+	}*/
 	pthread_mutex_lock(&mutx);
 	for(i=0; i<clnt_cnt; i++)
 		write(clnt_socks[i], msg, len);
 	pthread_mutex_unlock(&mutx);
 }
-
+/*
 int command_detection(char* msg){
 	char* command = "notice ";
 	int i=0, j=0;
@@ -111,7 +136,7 @@ int command_detection(char* msg){
 		return 0;
 	}
 }
-
+*/
 void error_handling(char * msg)
 {
 	fputs(msg, stderr);
